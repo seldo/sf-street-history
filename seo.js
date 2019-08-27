2a0cf634339484f8f4107dd6196b9ba8121bf0a5
Laurie Voss <github@seldo.com>
2019-08-27 12:39:19 -0700

    SEO shit

---
+++ seo.js
@@ -0,0 +1,22 @@
+const DEFAULT_SEO = {
+    title: "San Francisco Street Name History",
+    description: "A quick history of San Francisco told as a tour of various street and place names.",
+    openGraph: {
+      type: 'website',
+      locale: 'en_IE',
+      url: 'https://sf-street-history.seldo.now.sh/',
+      title: 'San Francisco Street Name History',
+      description: 'A quick history of San Francisco told as a tour of various street and place names.',
+      image:
+        'https://sf-street-history.seldo.now.sh/static/mission-dolores.png',
+      site_name: 'GaryMeehan.ie',
+      imageWidth: 1738,
+      imageHeight: 1782
+    },
+    twitter: {
+      handle: '@seldo',
+      cardType: 'summary_large_image'
+    }
+  };
+
+export default DEFAULT_SEO
